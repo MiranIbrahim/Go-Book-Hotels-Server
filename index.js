8138ecc60bf070ccd71b8b4e215c241d58fdd2b5
Miran Ibrahim <moeenuddin.miran@gmail.com>
2023-11-08 13:03:45 +0600

    jwt setup completed

--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
+
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  console.log("token: ", token);
+
+  if (!token) {
+    return res.status(401).send({ message: "unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden access" });
+    }
+    console.log("decoded", decoded);
+    req.decoded = decoded;
+    next();
+  });
+};
 
 // DB_USER  DB_PASS
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ivv8ial.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -21,7 +44,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -30,7 +53,70 @@ async function run() {
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+
+    const Rooms = client.db("GoBookHotelDB").collection("roomCollection");
+    const bookingCollection = client.db("GoBookHotelDB").collection("bookings");
+    // const CartTable = client.db("CarverseDB").collection("Cart");
+
+    // ---------------JWT----------------
+    app.post("/jwt", verifyToken, async (req, res) => {
+      const user = req.body;
+      console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res
+        .cookie("token", {
+          httpOnly: true,
+          secure: false,
+          semSite: "none",
+        })
+        .send({ success: true });
+    });
+
+    // ---------------- Getting All Rooms from Collection----------------------
+    app.get("/rooms", async (req, res) => {
+      try {
+        const cursor = Rooms.find();
+        const result = await cursor.toArray();
+        res.send(result);
+      } catch (error) {
+        console.log(error);
+        res.status(500).send({ message: "An error occurred while adding" });
+      }
+    });
+
+    app.post("/create-booking", verifyToken, async (req, res) => {
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    });
+
+    app.get("/bookings", verifyToken, async(req, res) => {
+      const queryEmail = req.query.email;
+      const tokenEmail = req.user.email;
+
+      if (queryEmail !== tokenEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      let query = {};
+      if (queryEmail) {
+        query.email = queryEmail;
+      }
+      const result = await bookingCollection.findOne({ email: queryEmail });
+      res.send(result);
+    });
+
+
+    app.delete("cancel-booking/:bookingId", verifyToken, async (req, res) => {
+      const id = req.params.bookingId;
+      const query = { _id: new ObjectId(id) };
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -38,13 +124,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-
-app.get('/', (req, res) => {
-  res.send('running...')
-})
+app.get("/", (req, res) => {
+  res.send("running...");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
